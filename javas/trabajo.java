4de218a11be58493641e813fc9f94d0e17872962
Andersson Cardenas <john.cardenasq@upb.edu.co>
2020-10-19 20:48:18 -0500

    codificacion menu principal

---
+++ javas/trabajo.java
@@ -0,0 +1,134 @@
+public class trabajo {
+
+	public static void limpiar(int lineas){
+		for (int i=0; i < lineas; i++){
+			System.out.println("");
+			}
+	}
+
+public static void main(String args[]) {
+
+ int decision=0;
+ int bucle=1;
+
+System.out.println("  ____________________________________________________   ");
+System.out.println(" |____________________________________________________|  ");
+System.out.println(" | __     __   ____   ___ ||  ____    ____     _  __  |  ");
+System.out.println(" ||  |__ |--|_| || |_|   |||_|**|*|__|+|+||___| ||  | |  ");
+System.out.println(" ||==|^^||--| |=||=| |=*=||| |~~|~|  |=|=|| | |~||==| |  ");
+System.out.println(" ||  |##||  | | || | |JRO|||-|  | |==|+|+||-|-|~||__| |  ");
+System.out.println(" ||__|__||__|_|_||_|_|___|||_|__|_|__|_|_||_|_|_||__|_|  ");
+System.out.println(" ||_______________________||__________________________|  ");
+System.out.println(" | _____________________  ||      __   __  _  __    _ |  ");
+System.out.println(" ||=|=|=|=|=|=|=|=|=|=|=| __..\\/ |  |_|  ||#||==|  / /|  ");
+System.out.println(" || | | | | | | | | | | |/\\ \\  \\\\|++|=|  || ||==| / / |  ");
+System.out.println(" ||_|_|_|_|_|_|_|_|_|_|_/_/\\_.___\\__|_|__||_||__|/_/__|  ");
+System.out.println(" |____________________ /\\~()/()~//\\ __________________|  ");
+System.out.println(" | __   __    _  _     \\_  (_ .  _/ _    ___     _____|  ");
+System.out.println(" ||~~|_|..|__| || |_ _   \\ //\\\\ /  |=|__|~|~|___| | | |  ");
+System.out.println(" ||--|+|^^|==|1||2| | |__/\\ __ /\\__| |==|x|x|+|+|=|=|=|  ");
+System.out.println(" ||__|_|__|__|_||_|_| /  \\ \\  / /  \\_|__|_|_|_|_|_|_|_|  ");
+System.out.println(" |_________________ _/    \\/\\/\\/    \\_ _______________|  ");
+System.out.println(" | _____   _   __  |/      \\../      \\|  __   __   ___|  ");
+System.out.println(" ||_____|_| |_|##|_||   |   \\/ __|   ||_|==|_|++|_|-|||  ");
+System.out.println(" ||______||=|#|--| |\\   \\   o    /   /| |  |~|  | | |||  ");
+System.out.println(" ||______||_|_|__|_|_\\   \\  o   /   /_|_|__|_|__|_|_|||  ");
+System.out.println(" |_________ __________\\___\\____/___/___________ ______|  ");
+System.out.println(" |__    _  /    ________     ______           /| _ _ _|  ");
+System.out.println(" |\\ \\  |=|/   //    /| //   /  /  / |        / ||%|%|%|  ");
+System.out.println(" | \\/\\ |*/  .//____//.//   /__/__/ (_)      /  ||=|=|=|  ");
+System.out.println("_|  \\/\\|/   /(____|/ //                    /  /||~|~|~|__");
+System.out.println(" |___\\_/   /________//   ________         /  / ||_|_|_|  ");
+System.out.println(" |___ /   (|________/   |\\_______\\       /  /| |______|  ");
+System.out.println("     /                  \\|________)     /  / | |         ");
+System.out.println();
+System.out.println("__________                                                                                                 ");
+System.out.println("\\______   \\__ __   ____   ____ _____    ______                                                             ");
+System.out.println(" |    |  _/  |  \\_/ __ \\ /    \\\\__  \\  /  ___/                                                             ");
+System.out.println(" |    |   \\  |  /\\  ___/|   |  \\/ __ \\_\\___ \\                                                              ");
+System.out.println(" |______  /____/  \\___  >___|  (____  /____  >                                                             ");
+System.out.println("        \\/            \\/     \\/     \\/     \\/                                                              ");
+
+
+ do{
+ System.out.println("                                         __           ____  ___");
+ System.out.println("_____________  ____ ___.__. ____   _____/  |_  ____   \\   \\/  /");
+ System.out.println("\\____ \\_  __ \\/  _ <   |  |/ __ \\_/ ___\\   __\\/  _ \\   \\     / ");
+ System.out.println("|  |_> >  | \\(  <_> )___  \\  ___/\\  \\___|  | (  <_> )  /     \\ ");
+ System.out.println("|   __/|__|   \\____// ____|\\___  >\\___  >__|  \\____/  /___/\\  \\");
+ System.out.println("|__|                \\/         \\/     \\/                    \\_/");
+ System.out.println();
+ System.out.println("| 1. Saludar                        |");
+ System.out.println("| 2. Ander papichulo                |");
+ System.out.println("| 3. Juego banderas                 |");
+ System.out.println("| 4. salir del menu                 |");
+ System.out.println("Digite que desea llevar");
+ 
+ decision= ConsoleInput.getInt();
+ 
+ switch(decision){
+ 
+ case 1:
+ System.out.println();
+ System.out.println("Her there I am using sublime");
+ 
+ break;
+ 
+ case 2: 
+ System.out.println();
+ System.out.println("Ander eres un severo papucho");
+ 
+ break;	
+ case 3: 
+ System.out.println();
+ System.out.println("¡¡vamonos al juegooo!!");
+ limpiar(56);
+System.out.println("   _____                       .__  __  .__         _____.__                       "); 
+System.out.println("_/ ____\\_ __  ____    __  _  _|__|/  |_|  |__    _/ ____\\  | _____     ____  ______");
+System.out.println("\\   __\\  |  \\/    \\   \\ \\/ \\/ /  \\   __\\  |  \\   \\   __\\|  | \\__  \\   / ___\\/  ___/");
+System.out.println(" |  | |  |  /   |  \\   \\     /|  ||  | |   Y  \\   |  |  |  |__/ __ \\_/ /_/  >___ \\ ");
+System.out.println(" |__| |____/|___|  /    \\/\\_/ |__||__| |___|  /   |__|  |____(____  /\\___  /____  >");
+System.out.println("                 \\/                         \\/                    \\//_____/     \\/"); 
+ System.out.println("Este maravilloso y entretenido juego consiste en llevar acabo una serie de preguntas \n estas preguntas apareceran en pantalla junto con unas opciones, debes elegir la respuesta que te parezca correcta \n así se irá llenando la bandera poco a poco e iras acumulando puntos \n si fallas disminuiran tus puntos y la bandera irá desapareciendo. ");
+ break;	
+ 
+ case 4:
+ bucle=0;
+ break;	
+
+ default:
+ System.out.println("Opcion no disponible");
+ bucle=0;
+ break;
+ 
+ }
+ System.out.println();
+ System.out.println("¿Otra opcion?, digita 0 para \"Si\" o digita 1 para \"No\" ");
+ bucle=ConsoleInput.getInt();
+ switch(bucle){
+ 	case 0:
+ 	System.out.println("¡¡¡Continuemos!!!!");
+ 	bucle=1;
+ 	break;
+
+ 	case 1:
+ 	System.out.println();
+ 	System.out.println("Esta bien, Gracias por jugar con nosotros");
+ 	bucle=0;
+ 	break;
+
+ 	default:
+ 	System.out.println("Opcion no disponible");
+ 	bucle=0;
+ 	break;
+ }
+ limpiar(56);
+ }while(bucle==1);
+ 
+
+ }
+
+ }
+
+
+
