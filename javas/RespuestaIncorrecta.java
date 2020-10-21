2dec4e54b3bcf0819bf1fe50732662523b77f90f
Luisa Alvarez Bello <luisa.alvarezb@upb.edu.co>
2020-10-21 16:14:41 -0500

    Version 7

--- javas/RespuestaIncorrecta.java
+++ javas/RespuestaIncorrecta.java
@@ -13,24 +13,24 @@ public class RespuestaIncorrecta{
 
 			case 1:
 
-				System.out.println(Console.Colors.RED+".___                                               __          ");
-				System.out.println(Console.Colors.RED+"|   | ____   ____  __________________  ____  _____/  |_  ____  ");
-				System.out.println(Console.Colors.RED+"|   |/    \\_/ ___\\/  _ \\_  __ \\_  __ \\/ __ \\/ ___\\   __\\/  _ \\ ");
-				System.out.println(Console.Colors.RED+"|   |   |  \\  \\__(  <_> )  | \\/|  | \\|  ___|  \\___|  | (  <_> )");
-				System.out.println(Console.Colors.RED+"|___|___|  /\\___  >____/|__|   |__|   \\___  >___  >__|  \\____/ ");
-				System.out.println(Console.Colors.RED+"         \\/     \\/                        \\/    \\/             ");
+				System.out.println(ConsoleColors.RED+".___                                               __          ");
+				System.out.println(ConsoleColors.RED+"|   | ____   ____  __________________  ____  _____/  |_  ____  ");
+				System.out.println(ConsoleColors.RED+"|   |/    \\_/ ___\\/  _ \\_  __ \\_  __ \\/ __ \\/ ___\\   __\\/  _ \\ ");
+				System.out.println(ConsoleColors.RED+"|   |   |  \\  \\__(  <_> )  | \\/|  | \\|  ___|  \\___|  | (  <_> )");
+				System.out.println(ConsoleColors.RED+"|___|___|  /\\___  >____/|__|   |__|   \\___  >___  >__|  \\____/ ");
+				System.out.println(ConsoleColors.RED+"         \\/     \\/                        \\/    \\/             ");
 
 			break;
 
 			case 2:
 
 
-				System.out.println(Console.Colors.RED+".___        __                 __               .___                                                ");
-				System.out.println(Console.Colors.RED+"|   | _____/  |_  ____   _____/  |______      __| _/____     ____  __ __  _______  ______           ");
-				System.out.println(Console.Colors.RED+"|   |/    \\   __\\/ __ \\ /    \\   __\\__  \\    / __ |/ __ \\   /    \\|  |  \\/ __ \\  \\/ /  _ \\     ");
-				System.out.println(Console.Colors.RED+"|   |   |  \\  | \\  ___/|   |  \\  |  / __ \\_ / /_/ \\  ___/  |   |  \\  |  |  ___/\\   (  <_> )         ");
-				System.out.println(Console.Colors.RED+"|___|___|  /__|  \\___  >___|  /__| (____  / \\____ |\\___  > |___|  /____/ \\___  >\\_/ \\____/ /\\ /\\ /\\ ");
-				System.out.println(Console.Colors.RED+"         \\/          \\/     \\/          \\/       \\/    \\/       \\/           \\/            \\/ \\/ \\/ ");
+				System.out.println(ConsoleColors.RED+".___        __                 __               .___                                                ");
+				System.out.println(ConsoleColors.RED+"|   | _____/  |_  ____   _____/  |______      __| _/____     ____  __ __  _______  ______           ");
+				System.out.println(ConsoleColors.RED+"|   |/    \\   __\\/ __ \\ /    \\   __\\__  \\    / __ |/ __ \\   /    \\|  |  \\/ __ \\  \\/ /  _ \\     ");
+				System.out.println(ConsoleColors.RED+"|   |   |  \\  | \\  ___/|   |  \\  |  / __ \\_ / /_/ \\  ___/  |   |  \\  |  |  ___/\\   (  <_> )         ");
+				System.out.println(ConsoleColors.RED+"|___|___|  /__|  \\___  >___|  /__| (____  / \\____ |\\___  > |___|  /____/ \\___  >\\_/ \\____/ /\\ /\\ /\\ ");
+				System.out.println(ConsoleColors.RED+"         \\/          \\/     \\/          \\/       \\/    \\/       \\/           \\/            \\/ \\/ \\/ ");
 			break;
 
 			case 3:
@@ -62,12 +62,12 @@ public class RespuestaIncorrecta{
 
 			default:
 
-			System.out.println(Console.Colors.RED+"________   __                                       ");
-			System.out.println(Console.Colors.RED+"\\_____  \\_/  |_____________    ___  __ ____ ________");
-			System.out.println(Console.Colors.RED+" /   |   \\   __\\_  __ \\__  \\   \\  \\/ // __ \\\\___   /");
-			System.out.println(Console.Colors.RED+"/    |    \\  |  |  | \\// __ \\_  \\   /\\  ___/ /    / ");
-			System.out.println(Console.Colors.RED+"\\_______  /__|  |__|  (____  /   \\_/  \\___  >_____ \\");
-			System.out.println(Console.Colors.RED+"        \\/                 \\/             \\/      \\/");
+			System.out.println(ConsoleColors.RED+"________   __                                       ");
+			System.out.println(ConsoleColors.RED+"\\_____  \\_/  |_____________    ___  __ ____ ________");
+			System.out.println(ConsoleColors.RED+" /   |   \\   __\\_  __ \\__  \\   \\  \\/ // __ \\\\___   /");
+			System.out.println(ConsoleColors.RED+"/    |    \\  |  |  | \\// __ \\_  \\   /\\  ___/ /    / ");
+			System.out.println(ConsoleColors.RED+"\\_______  /__|  |__|  (____  /   \\_/  \\___  >_____ \\");
+			System.out.println(ConsoleColors.RED+"        \\/                 \\/             \\/      \\/");
 
 			break;
 
