4fa232bfe7579abe0896bbcbf41174137af00654
Andersson Cardenas <john.cardenasq@upb.edu.co>
2020-10-22 13:19:04 -0500

    añadidura ascii

---
+++ Preguntas.java
@@ -0,0 +1,235 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+public class Preguntas{
+    
+	public static void main(String args[]) throws IOException{
+
+		    int i;
+
+			String preguntas[][], cadena;
+			
+			preguntas = new String[5][5]; //Crear matriz para preguntas.
+
+
+			String pais;
+
+			pais=Paises.devolverPais();
+
+			//System.out.println("El pais a buscar es: " + pais);
+
+			FileReader miArchivo = new FileReader("PreguntasPaises.csv"); //Clase que nos permite realizar lecturas de archivos de texto..
+			BufferedReader archivoLeido=new BufferedReader(miArchivo);//Permite leer texto de un InputStream de una forma sencilla. Permite leer caracteres, arrays y líneas.
+					
+
+			//lleno la matriz de vacios 
+			for(int m=0; m<5; m++ ){
+
+				for(int n=0; n<5; n++){
+						preguntas[m][n]=" ";
+				}
+			}
+
+			while((cadena = archivoLeido.readLine())!=null ) {
+	        	         	 
+	        	 String[] vector=cadena.split(",");
+
+	        	// System.out.println("Cadena: " + cadena);//imprimo la linea del archivo
+
+				if(pais.equals(vector[0])){ //Si el retorno del pais es igual al nombre en el segundo archivo
+
+					boolean esta= false; //Para determinar si un espacio del vector está ocupado.
+	        		
+	        		i=(int)(Math.random()*5); // Definir posicion aleatoria vacía paras ser llenada.
+
+	        		//System.out.println("i: " + i);//imprimo la i
+
+	        	 	while (!esta) {
+
+	        	 		//System.out.println("contenido matriz " + preguntas[i][0]);
+
+	        	 		if(preguntas[i][0].equals(" ")){
+
+	        	 			//System.out.println("Cadena: " + cadena);//imprimo la liena del archivo
+
+			        	 	preguntas[i][0]=vector[1];//El número se asigna a la matriz i,0
+							preguntas[i][1]=vector[2];//La pregunta se asigna a i,1
+							preguntas[i][2]=vector[3];//La respuesta correcta se asigna a i,2
+							preguntas[i][3]=vector[4];//La segunda opción se asigna a i,3
+							preguntas[i][4]=vector[5];//La otra opcion falsa se asigna a i,4
+
+							
+							esta=true;
+							//System.out.println("encontró una posición vacia" + i);//comentar
+	        	 		}
+	        	 		
+	        	 		i=(int)(Math.random()*5); //Asigna posicion aleatoria nuevamente.
+
+	        	 		//System.out.println("i: " + i);//imprimo la i
+	        	 		
+	        	 	}
+				}
+			}
+
+			
+			System.out.println();
+			System.out.println();
+			
+			for(int m=0; m<5; m++ ){//Punto de control.
+
+				for(int n=0; n<5; n++){
+						//System.out.print(preguntas[m][n] + " ");
+				}
+				System.out.println();
+			}		
+
+
+			//inicia el juego
+
+			//cambio el orden de las respuestas
+			String preguntas2[][];
+			preguntas2 = new String[5][5];
+			
+			
+
+			for(int m=0; m<5; m++ ){
+
+				for(int n=0; n<5; n++){
+						preguntas2[m][n]=" ";
+				}
+			}
+
+
+			for(int m=0; m<5; m++ ){
+
+				for(int n=0; n<5; n++){
+						
+						if (n==0 || n==1) {
+							preguntas2[m][n]=preguntas[m][n];
+						}
+
+						else {
+							
+									boolean esta= false;	
+									
+									i=(int)(Math.random()*3)+2;	
+										
+								//	System.out.println("i : " + i);
+									
+									while (!esta) {
+										
+										if(preguntas2[m][i].equals(" ")){
+												
+												preguntas2[m][i]=preguntas[m][n];
+												esta=true;
+												//System.out.println("encontr?una posici? vacia" + i);//comentar
+										}
+										
+										i=(int)(Math.random()*3)+2;	
+									//	System.out.println("i : " + i);
+									}
+						}
+					
+				}
+				
+			}	
+
+
+			//System.out.println();
+			//System.out.println();
+
+			for(int m=0; m<5; m++ ){//Punto de control
+
+				for(int n=0; n<5; n++){
+						//System.out.print(preguntas2[m][n] + " ");
+				}
+				//System.out.println();
+			}	
+
+			String rta="";
+			int correcta=0;
+			int score =0;
+			int incorrecta=0;
+			
+			boolean estaEnJuego=true;
+			
+
+			i=0;
+
+
+			while (estaEnJuego) {
+
+
+			System.out.println();
+			System.out.println("Pregunta: " + preguntas[i][1]);
+			System.out.println();
+
+			
+		//	for(int m=0; m<5; m++ ){
+
+				for(int n=2; n<5; n++){
+
+						System.out.print(preguntas2[i][n] + " ");
+					}
+				
+				System.out.println();
+    			//System.out.print("Opciones: " + preguntas2[][]);
+    			Scanner reader= new Scanner(System.in);
+    			rta = reader.nextLine();
+			    
+    			if(rta.equals(preguntas[i][2])){
+    				
+    				System.out.println("Correcto");
+    				correcta= correcta + 1;
+    				score = score + 50;
+
+    				//código para imprimir la bandera
+
+    				if (correcta==4) {
+			    		//System.out.println("YOU WIN!!");
+			    		estaEnJuego=false;
+			    		System.out.println("vsgd");
+		    		}
+
+    			}
+    			else{
+    				System.out.println("Incorrecto");
+    				incorrecta= incorrecta + 1;
+    				score = score -50;
+
+
+    				if (incorrecta==2) {
+			    		System.out.println("Game Over");
+			    		estaEnJuego=false;
+
+		    		}
+
+		    		else if(rta.equals("x")){
+			    		System.out.println("Saliste del juego.");
+			    		estaEnJuego=false;
+
+
+
+		    		}
+    			}
+		   //}
+
+				i=i+1;
+
+
+
+		    System.out.println("Score: " + score + "        Correct: " + correcta + "        Incorrect: " + incorrecta);
+
+
+
+
+		}
+    }
+}			
+
+
+
---
+++ funcionb.java
@@ -0,0 +1,604 @@
+public class funcionb{
+
+public static int aleatorio(int a){
+
+int c= (int)(Math.random()*a);
+
+return c;
+
+}
+
+public static void bandera(int a){
+String[] bandera= ConsoleFile.read("recursos/info_banderas.csv");
+if (a==1) {
+	for (int j=0;j<20 ;j++ ) {
+
+String[] separador= bandera[j].split(";"); 
+
+	for (int s=0;s<separador.length ;s++ ) {
+
+		switch(separador[s]){
+
+			case "1":
+			System.out.print(ConsoleColors.RED_BACKGROUND+" ");
+			break;
+			case "2":
+			System.out.print(ConsoleColors.BLUE_BACKGROUND+" ");
+			break;
+			case "3":
+			System.out.print(ConsoleColors.WHITE_BACKGROUND+" ");
+			break;
+		}
+	}
+
+System.out.print(ConsoleColors.RESET);
+	System.out.println();
+
+}
+}
+
+if (a==2) {
+	for (int j=20;j<40 ;j++ ) {
+		String[] separador= bandera[j].split(";"); 
+	for (int s=0;s<separador.length;s++ ) {
+		switch(separador[s]){
+		case "1":
+			System.out.print(ConsoleColors.RED_BACKGROUND+" ");
+			break;
+			case "2":
+			System.out.print(ConsoleColors.BLUE_BACKGROUND+" ");
+			break;
+			case "4":
+			System.out.print(ConsoleColors.YELLOW_BACKGROUND+" ");
+			break;
+		}
+	}
+	System.out.print(ConsoleColors.RESET);
+	System.out.println();
+	
+}
+}
+if (a==3) {
+	for (int j=40;j<60 ;j++ ) {
+		String[] separador= bandera[j].split(";"); 
+	for (int s=0;s<separador.length;s++ ) {
+		switch(separador[s]){
+		case "1":
+			System.out.print(ConsoleColors.RED_BACKGROUND+" ");
+			break;
+			case "3":
+			System.out.print(ConsoleColors.WHITE_BACKGROUND+" ");
+			break;
+		}}
+	System.out.print(ConsoleColors.RESET);
+	System.out.println();
+	
+}
+}
+if (a==4) {
+	for (int j=60;j<80 ;j++ ) {
+		String[] separador= bandera[j].split(";"); 
+	for (int s=0;s<separador.length;s++ ) {
+		switch(separador[s]){
+			case "1":
+			System.out.print(ConsoleColors.RED_BACKGROUND+" ");
+			break;
+			case "3":
+			System.out.print(ConsoleColors.WHITE_BACKGROUND+" ");
+			break;
+		}}
+	System.out.print(ConsoleColors.RESET);
+	System.out.println();
+	
+}
+}
+if (a==5) {
+	for (int j=80;j<100 ;j++ ) {
+		String[] separador= bandera[j].split(";"); 
+	for (int s=0;s<separador.length;s++ ) {
+		switch(separador[s]){
+			case "1":
+			System.out.print(ConsoleColors.RED_BACKGROUND+" ");
+			break;
+			case "3":
+			System.out.print(ConsoleColors.WHITE_BACKGROUND+" ");
+			break;
+		}}
+	System.out.print(ConsoleColors.RESET);
+	System.out.println();
+	
+}
+}
+if (a==6) {
+	for (int j=100;j<120 ;j++ ) {
+		String[] separador= bandera[j].split(";"); 
+	for (int s=0;s<separador.length;s++ ) {
+		switch(separador[s]){
+		case "1":
+			System.out.print(ConsoleColors.RED_BACKGROUND+" ");
+			break;
+			case "3":
+			System.out.print(ConsoleColors.WHITE_BACKGROUND+" ");
+			break;
+			case "5":
+			System.out.print(ConsoleColors.GREEN_BACKGROUND+" ");
+			break;
+		}}
+	System.out.print(ConsoleColors.RESET);
+	System.out.println();
+	
+}
+}
+if (a==7) {
+	for (int j=120;j<140 ;j++ ) {
+		String[] separador= bandera[j].split(";"); 
+	for (int s=0;s<separador.length;s++ ) {
+		switch(separador[s]){
+		case "3":
+			System.out.print(ConsoleColors.WHITE_BACKGROUND+" ");
+			break;
+			case "5":
+			System.out.print(ConsoleColors.GREEN_BACKGROUND+" ");
+			break;
+		}}
+	System.out.print(ConsoleColors.RESET);
+	System.out.println();
+	
+}
+}
+if (a==8) {
+	for (int j=140;j<160 ;j++ ) {
+		String[] separador= bandera[j].split(";"); 
+	for (int s=0;s<separador.length;s++ ) {
+		switch(separador[s]){
+					case "1":
+			System.out.print(ConsoleColors.RED_BACKGROUND+" ");
+			break;
+			case "2":
+			System.out.print(ConsoleColors.BLUE_BACKGROUND+" ");
+			break;
+			case "3":
+			System.out.print(ConsoleColors.WHITE_BACKGROUND+" ");
+			break;
+		}}
+	System.out.print(ConsoleColors.RESET);
+	System.out.println();
+	
+}
+}
+if (a==9) {
+	for (int j=160;j<180 ;j++ ) {
+		String[] separador= bandera[j].split(";"); 
+	for (int s=0;s<separador.length;s++ ) {
+		switch(separador[s]){
+					case "1":
+			System.out.print(ConsoleColors.RED_BACKGROUND+" ");
+			break;
+			case "3":
+			System.out.print(ConsoleColors.WHITE_BACKGROUND+" ");
+			break;
+		}
+		}
+	System.out.print(ConsoleColors.RESET);
+	System.out.println();
+	
+}
+}
+if (a==10) {
+	for (int j=180;j<200 ;j++ ) {
+		String[] separador= bandera[j].split(";"); 
+	for (int s=0;s<separador.length;s++ ) {
+		switch(separador[s]){
+			case "1":
+			System.out.print(ConsoleColors.RED_BACKGROUND+" ");
+			break;
+			case "2":
+			System.out.print(ConsoleColors.BLUE_BACKGROUND+" ");
+			break;
+			case "4":
+			System.out.print(ConsoleColors.YELLOW_BACKGROUND+" ");
+			break;
+		}}
+		
+	System.out.print(ConsoleColors.RESET);
+	System.out.println();
+	
+}
+}
+if (a==11) {
+	for (int j=200;j<220 ;j++ ) {
+		String[] separador= bandera[j].split(";"); 
+	for (int s=0;s<separador.length;s++ ) {
+		switch(separador[s]){
+					case "1":
+			System.out.print(ConsoleColors.RED_BACKGROUND+" ");
+			break;
+			case "4":
+			System.out.print(ConsoleColors.YELLOW_BACKGROUND+" ");
+			break;
+			case "8":
+			System.out.print(ConsoleColors.BLACK_BACKGROUND+" ");
+			break;
+		}}
+	System.out.print(ConsoleColors.RESET);
+	System.out.println();
+	
+}
+}
+if (a==12) {
+	for (int j=220;j<240 ;j++ ) {
+		String[] separador= bandera[j].split(";"); 
+	for (int s=0;s<separador.length;s++ ) {
+		switch(separador[s]){
+		case "1":
+			System.out.print(ConsoleColors.RED_BACKGROUND+" ");
+			break;
+			case "3":
+			System.out.print(ConsoleColors.WHITE_BACKGROUND+" ");
+			break;
+		}}
+	System.out.print(ConsoleColors.RESET);
+	System.out.println();
+	
+}
+}
+if (a==13) {
+	for (int j=240;j<260 ;j++ ) {
+		String[] separador= bandera[j].split(";"); 
+	for (int s=0;s<separador.length;s++ ) {
+		switch(separador[s]){
+		case "1":
+			System.out.print(ConsoleColors.RED_BACKGROUND+" ");
+			break;
+			case "2":
+			System.out.print(ConsoleColors.BLUE_BACKGROUND+" ");
+			break;
+			case "4":
+			System.out.print(ConsoleColors.YELLOW_BACKGROUND+" ");
+			break;
+		}}
+	System.out.print(ConsoleColors.RESET);
+	System.out.println();
+	
+}
+}
+if (a==14) {
+	for (int j=260;j<280 ;j++ ) {
+		String[] separador= bandera[j].split(";"); 
+	for (int s=0;s<separador.length;s++ ) {
+		switch(separador[s]){
+			case "1":
+			System.out.print(ConsoleColors.RED_BACKGROUND+" ");
+			break;
+			case "3":
+			System.out.print(ConsoleColors.WHITE_BACKGROUND+" ");
+			break;
+			case "8":
+			System.out.print(ConsoleColors.BLACK_BACKGROUND+" ");
+			break;
+		}}
+	System.out.print(ConsoleColors.RESET);
+	System.out.println();
+	
+}
+}
+if (a==15) {
+	for (int j=280;j<300 ;j++ ) {
+		String[] separador= bandera[j].split(";"); 
+	for (int s=0;s<separador.length;s++ ) {
+		switch(separador[s]){
+		case "1":
+			System.out.print(ConsoleColors.RED_BACKGROUND+" ");
+			break;
+			case "2":
+			System.out.print(ConsoleColors.BLUE_BACKGROUND+" ");
+			break;
+			case "3":
+			System.out.print(ConsoleColors.WHITE_BACKGROUND+" ");
+			break;
+			case "4":
+			System.out.print(ConsoleColors.YELLOW_BACKGROUND+" ");
+			break;
+		}}
+	System.out.print(ConsoleColors.RESET);
+	System.out.println();
+	
+}
+}
+if (a==16) {
+	for (int j=300;j<320 ;j++ ) {
+		String[] separador= bandera[j].split(";"); 
+	for (int s=0;s<separador.length;s++ ) {
+		switch(separador[s]){
+		case "1":
+			System.out.print(ConsoleColors.RED_BACKGROUND+" ");
+			break;
+			case "3":
+			System.out.print(ConsoleColors.WHITE_BACKGROUND+" ");
+			break;
+			case "4":
+			System.out.print(ConsoleColors.YELLOW_BACKGROUND+" ");
+			break;
+			case "8":
+			System.out.print(ConsoleColors.BLACK_BACKGROUND+" ");
+			break;
+		}}
+	System.out.print(ConsoleColors.RESET);
+	System.out.println();
+	
+}
+}
+if (a==17) {
+	for (int j=320;j<340 ;j++ ) {
+		String[] separador= bandera[j].split(";"); 
+	for (int s=0;s<separador.length;s++ ) {
+		switch(separador[s]){
+		case "1":
+			System.out.print(ConsoleColors.RED_BACKGROUND+" ");
+			break;
+			case "3":
+			System.out.print(ConsoleColors.WHITE_BACKGROUND+" ");
+			break;
+			case "8":
+			System.out.print(ConsoleColors.BLACK_BACKGROUND+" ");
+			break;
+			case "5":
+			System.out.print(ConsoleColors.GREEN_BACKGROUND+" ");
+			break;
+		}}
+	System.out.print(ConsoleColors.RESET);
+	System.out.println();
+	
+}
+}
+if (a==18) {
+	for (int j=340;j<360 ;j++ ) {
+		String[] separador= bandera[j].split(";"); 
+	for (int s=0;s<separador.length;s++ ) {
+		switch(separador[s]){
+		case "1":
+			System.out.print(ConsoleColors.RED_BACKGROUND+" ");
+			break;
+			case "2":
+			System.out.print(ConsoleColors.BLUE_BACKGROUND+" ");
+			break;
+			case "3":
+			System.out.print(ConsoleColors.WHITE_BACKGROUND+" ");
+			break;
+		}}
+	System.out.print(ConsoleColors.RESET);
+	System.out.println();
+	
+}
+}
+if (a==19) {
+	for (int j=360;j<380 ;j++ ) {
+		String[] separador= bandera[j].split(";"); 
+	for (int s=0;s<separador.length;s++ ) {
+		switch(separador[s]){
+		case "1":
+			System.out.print(ConsoleColors.RED_BACKGROUND+" ");
+			break;
+			case "2":
+			System.out.print(ConsoleColors.BLUE_BACKGROUND+" ");
+			break;
+			case "3":
+			System.out.print(ConsoleColors.WHITE_BACKGROUND+" ");
+			break;
+		}}
+	System.out.print(ConsoleColors.RESET);
+	System.out.println();
+	
+}
+}
+if (a==20) {
+	for (int j=380;j<400 ;j++ ) {
+		String[] separador= bandera[j].split(";"); 
+	for (int s=0;s<separador.length;s++ ) {
+		switch(separador[s]){
+		case "2":
+			System.out.print(ConsoleColors.BLUE_BACKGROUND+" ");
+			break;
+			case "3":
+			System.out.print(ConsoleColors.WHITE_BACKGROUND+" ");
+			break;
+		}}
+	System.out.print(ConsoleColors.RESET);
+	System.out.println();
+	
+}
+}
+if (a==21) {
+	for (int j=400;j<420 ;j++ ) {
+		String[] separador= bandera[j].split(";"); 
+	for (int s=0;s<separador.length;s++ ) {
+		switch(separador[s]){
+		case "1":
+			System.out.print(ConsoleColors.RED_BACKGROUND+" ");
+			break;
+			case "2":
+			System.out.print(ConsoleColors.BLUE_BACKGROUND+" ");
+			break;
+			case "3":
+			System.out.print(ConsoleColors.WHITE_BACKGROUND+" ");
+			break;
+		}}
+	System.out.print(ConsoleColors.RESET);
+	System.out.println();
+	
+}
+}
+if (a==22) {
+	for (int j=420;j<440 ;j++ ) {
+		String[] separador= bandera[j].split(";"); 
+	for (int s=0;s<separador.length;s++ ) {
+		switch(separador[s]){
+		case "1":
+			System.out.print(ConsoleColors.RED_BACKGROUND+" ");
+			break;
+			case "2":
+			System.out.print(ConsoleColors.BLUE_BACKGROUND+" ");
+			break;
+			case "3":
+			System.out.print(ConsoleColors.WHITE_BACKGROUND+" ");
+			break;
+		}}
+	System.out.print(ConsoleColors.RESET);
+	System.out.println();
+	
+}
+}
+if (a==23) {
+	for (int j=440;j<460 ;j++ ) {
+		String[] separador= bandera[j].split(";"); 
+	for (int s=0;s<separador.length;s++ ) {
+		switch(separador[s]){
+		case "1":
+			System.out.print(ConsoleColors.RED_BACKGROUND+" ");
+			break;
+			case "4":
+			System.out.print(ConsoleColors.YELLOW_BACKGROUND+" ");
+			break;
+			case "5":
+			System.out.print(ConsoleColors.GREEN_BACKGROUND+" ");
+			break;
+		}}
+	System.out.print(ConsoleColors.RESET);
+	System.out.println();
+	
+}
+}
+if (a==24) {
+	for (int j=460;j<480 ;j++ ) {
+		String[] separador= bandera[j].split(";"); 
+	for (int s=0;s<separador.length;s++ ) {
+		switch(separador[s]){
+		case "1":
+			System.out.print(ConsoleColors.RED_BACKGROUND+" ");
+			break;
+			case "2":
+			System.out.print(ConsoleColors.BLUE_BACKGROUND+" ");
+			break;
+			case "3":
+			System.out.print(ConsoleColors.WHITE_BACKGROUND+" ");
+			break;
+			case "5":
+			System.out.print(ConsoleColors.GREEN_BACKGROUND+" ");
+			break;
+		}}
+	System.out.print(ConsoleColors.RESET);
+	System.out.println();
+	
+}
+}
+if (a==25) {
+for (int j=480;j<500 ;j++ ) {
+		String[] separador= bandera[j].split(";"); 
+	for (int s=0;s<separador.length;s++ ) {
+		switch(separador[s]){
+		case "1":
+			System.out.print(ConsoleColors.RED_BACKGROUND+" ");
+			break;
+			case "2":
+			System.out.print(ConsoleColors.BLUE_BACKGROUND+" ");
+			break;
+			case "3":
+			System.out.print(ConsoleColors.WHITE_BACKGROUND+" ");
+			break;
+		}
+}
+	System.out.print(ConsoleColors.RESET);
+	System.out.println();
+	
+}
+}
+if (a==26) {
+for (int j=500;j<520 ;j++ ) {
+		String[] separador= bandera[j].split(";"); 
+	for (int s=0;s<separador.length;s++ ) {
+		switch(separador[s]){
+		case "1":
+			System.out.print(ConsoleColors.RED_BACKGROUND+" ");
+			break;
+			case "2":
+			System.out.print(ConsoleColors.BLUE_BACKGROUND+" ");
+			break;
+			case "3":
+			System.out.print(ConsoleColors.WHITE_BACKGROUND+" ");
+			break;
+			case "4":
+			System.out.print(ConsoleColors.YELLOW_BACKGROUND+" ");
+			break;
+			case "5":
+			System.out.print(ConsoleColors.GREEN_BACKGROUND+" ");
+			break;
+			case "6":
+			System.out.print(ConsoleColors.PURPLE_BACKGROUND+" ");
+			break;
+			case "7":
+			System.out.print(ConsoleColors.CYAN_BACKGROUND+" ");
+			break;
+			case "8":
+			System.out.print(ConsoleColors.BLACK_BACKGROUND+" ");
+			break;
+		}
+}
+	System.out.print(ConsoleColors.RESET);
+	System.out.println();
+	
+}
+}
+if (a==27) {
+for (int j=520;j<540 ;j++ ) {
+		String[] separador= bandera[j].split(";"); 
+	for (int s=0;s<separador.length;s++ ) {
+		switch(separador[s]){
+		case "1":
+			System.out.print(ConsoleColors.RED_BACKGROUND+" ");
+			break;
+			case "2":
+			System.out.print(ConsoleColors.BLUE_BACKGROUND+" ");
+			break;
+			case "3":
+			System.out.print(ConsoleColors.WHITE_BACKGROUND+" ");
+			break;
+			case "4":
+			System.out.print(ConsoleColors.YELLOW_BACKGROUND+" ");
+			break;
+			case "5":
+			System.out.print(ConsoleColors.GREEN_BACKGROUND+" ");
+			break;
+			case "6":
+			System.out.print(ConsoleColors.PURPLE_BACKGROUND+" ");
+			break;
+			case "7":
+			System.out.print(ConsoleColors.CYAN_BACKGROUND+" ");
+			break;
+			case "8":
+			System.out.print(ConsoleColors.BLACK_BACKGROUND+" ");
+			break;
+		}
+}
+	System.out.print(ConsoleColors.RESET);
+	System.out.println();
+	
+}
+}
+}
+	public static void main(String[] args) {
+
+		bandera(aleatorio(25));
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
